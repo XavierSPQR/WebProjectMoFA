40e469a7f79499ff20d30cd28cc4d54c67ac809b
PunsaraniMadusha <saranidharmasena2001@gmail.com>
2025-12-25 01:13:09 +0530

    change footer color footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-800 text-white py-4 mt-auto">
+    <footer className="bg-[#F4D06F] text-gray-900 py-4 mt-auto">
       <div className="container mx-auto text-center">
         <p>
           Copyrights &copy; 2025. Foreign Ministry. All Rights Reserved. Developed by Wayamba University
