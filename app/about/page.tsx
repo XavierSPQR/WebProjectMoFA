cfee8b06f5770ae046de94e35a22289ba1fdd5bf
PunsaraniMadusha <saranidharmasena2001@gmail.com>
2026-02-23 09:57:12 +0530

    change comm

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -23,7 +23,7 @@ export default function Page() {
           </p>
         </div>
 
-        {/* Profiles Section */}
+        {/* Profiles */}
         <div className="bg-gray-100 p-6 rounded-lg">
           <h2 className="text-2xl font-bold mb-4">Profiles</h2>
           <ul className="space-y-2 list-none">
